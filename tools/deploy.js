c315389c8c9e5a540cbeb267b1ff633828d38d49
James Frank <james.frank@nih.gov>
2023-04-27 14:55:53 -0400

    (#1) Set up initial workflows

--- tools/deploy.js
+++ tools/deploy.js
@@ -16,8 +16,7 @@ const envParams = Object.entries(process.env).filter(([key, value]) =>
         `${o[0]} ${o[1]}`).join(' ')
 
 try {
-	console.log('options ' + options + ' ' + envParams)
-    exeCute('./tools/Akumina.SiteDeployer.exe options ' + options + ' ' + envParams);
+    exeCute('.\\tools\\Akumina.SiteDeployer.exe options ' + options + ' ' + envParams);
 } catch (ex) {
     console.error('Error executing the SiteDeployer.exe, check console');
 }
