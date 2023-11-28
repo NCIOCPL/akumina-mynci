b5c985ee6de07a9aa3ff24dca24ef09323427478
James Frank <james.frank@nih.gov>
2023-11-27 21:25:16 -0500

    (#12) Read content from XML files
    Touches #12

--- migration/src/lib/drupal-content.js
+++ migration/src/lib/drupal-content.js
@@ -1,8 +1,31 @@
+import xml2js from 'xml2js';
+import { promises as fsPromises } from 'fs';
+
 /**
  * Represents an extraction of content from the Drupal 7 site
  *
  */
 
-class DrupalContent {}
+class DrupalContent {
+  /**
+   * Load content from XML export.
+   *
+   * @param {string} file A local filename for reading content from.
+   */
+  async loadContent(file) {
+    try {
+      // TBD: Reference actual files
+      const filePath = './__mocks/export_content.xml';
+      const xmlData = await fsPromises.readFile(filePath);
+
+      const parser = new xml2js.Parser();
+      const results = await parser.parseStringPromise(xmlData);
+
+      console.log(results.nodes);
+    } catch (err) {
+      console.error('Error:', err);
+    }
+  }
+}
 
 export default DrupalContent;
