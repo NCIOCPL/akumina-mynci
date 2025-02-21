30fc7b032fce1e0186b7040079e5a5660aaf6fc5
James Frank <james.frank@nih.gov>
2025-02-21 10:58:14 -0700

    (#90) Move image gallery to using new thumbnail URL
    Will need future improvements to pull URL from the API.
    
    Closes #90

--- site/src/js/library/digitalworkplace.custom.js
+++ site/src/js/library/digitalworkplace.custom.js
@@ -43,25 +43,20 @@ Handlebars.registerHelper("ThumbnailImage", function (image, options) {
     return image
 });
 
+// Used with the Latest Media Widget / ncifeaturedphotos.html view
 Handlebars.registerHelper("MediumThumbnailImage", function (image, exclusion, options) {
     if (image != null && image.toLowerCase().indexOf("akumina%20library") == -1){
-        var imageSegments = image.split("/");
-        imageSegments.splice(imageSegments.length - 1, 0, "_w");
-        var fileName = imageSegments[imageSegments.length - 1];
-        var fileNameSegments = fileName.toLowerCase().split('.');
-        var fileExtValue = fileNameSegments[fileNameSegments.length - 1];
-        var fileExtValueSegments = fileExtValue.split("&");
-        var fileExt = fileExtValueSegments[0];
-        var useThumbnail = true;
-        if (exclusion != null && Array.isArray(exclusion)){ // check excluded extensions
-            var onExclusionList = $.inArray(fileExt, exclusion) > -1;
-            useThumbnail = !onExclusionList;
-        }
-        if (useThumbnail){
-            fileName = fileName.replace("." + fileExt,"_" + fileExt + ".jpg");
-            imageSegments[imageSegments.length - 1] =  fileName;
-            image = imageSegments.join("/");
-        }
+        var imagePath = image.split("relativeurl=")[1];
+        //var useThumbnail = true;
+        //if (exclusion != null && Array.isArray(exclusion)){ // check excluded extensions
+        //    var onExclusionList = $.inArray(fileExt, exclusion) > -1;
+        //    useThumbnail = !onExclusionList;
+        //}
+        //if (useThumbnail){
+
+            // To-Do: Change the tenant URL to reference a variable
+            image = "https://nih.sharepoint.com/_layouts/15/getpreview.ashx?path=" + imagePath + "&resolution=0";
+        //}
     }
     return image
 });
