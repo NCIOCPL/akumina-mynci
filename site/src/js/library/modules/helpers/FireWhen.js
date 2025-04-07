9c6afebef133c7148cdf4c3144b43dbd0e5c660c
Bela Olson <bela.olson@nih.gov>
2025-04-07 14:46:09 -0400

    (#136) Move FireWhen to a module
    * added helper folder for FireWhen.js
    * Replaced instances of window.FireWhen to instead call new FireWhen module
    
    Closes #136

---
+++ site/src/js/library/modules/helpers/FireWhen.js
@@ -0,0 +1,20 @@
+export function FireWhen(id, condition, callback, testInterval) {
+    if (condition == null) {
+      return;
+    }
+    var conditionMet = condition();
+    if (conditionMet) {
+      clearTimeout(window[id]);
+      Akumina.AddIn.Logger.WriteInfoLog('FireWhen:conditionMet:' + id);
+      callback.apply(this, Array.prototype.slice.call(arguments, 4));
+    } else {
+      clearTimeout(window[id]);
+      window[id] = window.setTimeout(
+        function (args) {
+          window.FireWhen.apply(this, args);
+        },
+        testInterval,
+        arguments
+      );
+    }
+  };
