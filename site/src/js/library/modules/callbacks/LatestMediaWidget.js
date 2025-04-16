75cffc604559f6d6c243d1af536c89d223bca342
Bela Olson <bela.olson@nih.gov>
2025-04-16 15:07:28 -0400

    (#130) Fix ESLint Errors
    * adjusted code to fix various eslint errors
    
    Closes #130
    Closes #131

--- site/src/js/library/modules/callbacks/LatestMediaWidget.js
+++ site/src/js/library/modules/callbacks/LatestMediaWidget.js
@@ -1,54 +1,8 @@
 import { FireWhen } from "../helpers/FireWhen";
 
 export function latestMediaWidget_callbacks() {
-
-    function Slider(obj) {
-        this.images = $(obj.images);
-        this.auto = obj.auto;
-        this.btnPrev = obj.btnPrev;
-        this.btnNext = obj.btnNext;
-        this.rate = obj.rate || 1000;
     
-        var i = 0;
-        var slider = this;
-    
-        // The "Previous" button: to remove the class .shoved, show the previous image and add the .shoved class
-        this.prev = function () {
-          slider.images.eq(i).removeClass('shown');
-          i--;
-    
-          if (i < 0) {
-            i = slider.images.length - 1;
-          }
-          slider.images.eq(i).addClass('shown');
-        };
-    
-        // The "Next" button: to remove the class .shoved, show the next image and add the .shoved class
-        this.next = function () {
-          slider.images.eq(i).removeClass('shown');
-          i++;
-    
-          if (i >= slider.images.length) {
-            i = 0;
-          }
-          slider.images.eq(i).addClass('shown');
-        };
-    
-        // To add next and prev functions when clicking the corresponding buttons
-        $(slider.btnPrev).on('click', function () {
-          slider.prev();
-        });
-        $(slider.btnNext).on('click', function () {
-          slider.next();
-        });
-    
-        // For the automatic slider: this method calls the next function at the set rate
-        if (slider.auto) {
-          setInterval(slider.next, slider.rate);
-        }
-    }
-    
-    window.LatestMediaWidget_data_loadPhotos = function (widget, properties) {
+    window.LatestMediaWidget_data_loadPhotos = function (_widget, _properties) {
         if (window['NCI_LatestMediaPhotos_load'] == null) {
             var prefix =
                 Akumina.Digispace.ConfigurationContext.FrameworkCDNPrefix +
@@ -62,7 +16,7 @@ export function latestMediaWidget_callbacks() {
                 function () {
                     console.log('success loading ' + slickSliderUrl);
                 },
-                function (e) {
+                function (_e) {
                     console.log('error loading ' + slickSliderUrl);
                 }
                 );
