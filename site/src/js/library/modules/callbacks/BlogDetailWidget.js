f4731ae590c67e5d937264f5d4895f1725341ff0
James Frank <james.frank@nih.gov>
2025-04-18 06:33:25 -0600

    (#145) Dynamically assign callbacks to window object
    Closes #145

--- site/src/js/library/modules/callbacks/BlogDetailWidget.js
+++ site/src/js/library/modules/callbacks/BlogDetailWidget.js
@@ -1,23 +1,22 @@
-export function blogDetailWidget_callbacks() {
-  /**
-   * Data callback for Blog Detail Widget. Does the following:
-   *
-   * - Updates displayed date field to be the actual date
-   *
-   * @param {object} data The widget data after being retrieved from SharePoint
-   * @returns {object} Updated widget data
-   */
-  window.blogDetail_data_changeDate = function (data) {
-    // Update article published date from "X days ago" to actual date
-    if (data.newsarticle.publisheddate != '') {
-      const date = new Date(data.newsarticle.Publish_x0020_Date);
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      data.newsarticle.publisheddate = date.toLocaleDateString(
-        'en-US',
-        options
-      );
-    }
+/**
+ * Data callback for Blog Detail Widget. Does the following:
+ *
+ * - Updates displayed date field to be the actual date
+ *
+ * @param {object} data The widget data after being retrieved from SharePoint
+ * @returns {object} Updated widget data
+ */
+const blogDetail_data_changeDate = function (data) {
+  // Update article published date from "X days ago" to actual date
+  if (data.newsarticle.publisheddate != '') {
+    const date = new Date(data.newsarticle.Publish_x0020_Date);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    data.newsarticle.publisheddate = date.toLocaleDateString('en-US', options);
+  }
 
-    return data;
-  };
+  return data;
+};
+
+export const blogDetailWidget_callbacks = {
+  blogDetail_data_changeDate
 }
