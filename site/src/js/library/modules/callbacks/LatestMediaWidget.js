3ecd4d9a6252b3e69ede3e7a1ffce6abab9809f0
James Frank <james.frank@nih.gov>
2025-04-18 06:33:25 -0600

    (#145) Dynamically assign callbacks to window object
    Closes #145

--- site/src/js/library/modules/callbacks/LatestMediaWidget.js
+++ site/src/js/library/modules/callbacks/LatestMediaWidget.js
@@ -1,63 +1,64 @@
 import { FireWhen } from "../helpers/FireWhen";
 
-export function latestMediaWidget_callbacks() {
-    
-    window.LatestMediaWidget_data_loadPhotos = function (_widget, _properties) {
-        if (window['NCI_LatestMediaPhotos_load'] == null) {
-            var prefix =
-                Akumina.Digispace.ConfigurationContext.FrameworkCDNPrefix +
-                '/fe/' +
-                Akumina.Digispace.Version;
-            var suffix = '?v=' + Akumina.Digispace.SiteContext.ImplementationVersion;
-            var slickSliderUrl = prefix + '/vendor/slickslider.min.js' + suffix;
-            new Akumina.Digispace.Data.CacheManager()
-                .cachedScript(slickSliderUrl)
-                .then(
-                function () {
-                    console.log('success loading ' + slickSliderUrl);
-                },
-                function (_e) {
-                    console.log('error loading ' + slickSliderUrl);
-                }
-                );
-            window['NCI_LatestMediaPhotos_load'] = true;
-        }
+const latestMediaWidget_data_loadPhotos = function (_widget, _properties) {
+    if (window['NCI_LatestMediaPhotos_load'] == null) {
+        var prefix =
+            Akumina.Digispace.ConfigurationContext.FrameworkCDNPrefix +
+            '/fe/' +
+            Akumina.Digispace.Version;
+        var suffix = '?v=' + Akumina.Digispace.SiteContext.ImplementationVersion;
+        var slickSliderUrl = prefix + '/vendor/slickslider.min.js' + suffix;
+        new Akumina.Digispace.Data.CacheManager()
+            .cachedScript(slickSliderUrl)
+            .then(
+            function () {
+                console.log('success loading ' + slickSliderUrl);
+            },
+            function (_e) {
+                console.log('error loading ' + slickSliderUrl);
+            }
+            );
+        window['NCI_LatestMediaPhotos_load'] = true;
+    }
 
-        FireWhen(
-          'NCI_LatestMediaPhotos'.toLocaleLowerCase(),
-          function () {
-            return jQuery().slick != null;
-          },
-          function () {
-            $(document).ready(function () {
-              $('.featuredPhotosSlider').slick({
-                dots: true,
-                infinite: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-    
-                speed: 500,
-                fade: true,
-                cssEase: 'linear',
-    
-                autoplay: true,
-                autoplayspeed: 5000,
-              });
-            });
-    
-            $('.ia-modal-inline-trigger-photo').magnificPopup({
-              type: 'inline',
-              preloader: false,
-              closeBtnInside: true,
-              showCloseBtn: true,
-              fixedBgPos: true,
-              mainClass: 'ak-photo-modal',
-            });
-            $('.ia-modal-inline-trigger-photo').on('click', function () {
-              $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-            });
-          },
-          250
-        );
-    };
+    FireWhen(
+      'NCI_LatestMediaPhotos'.toLocaleLowerCase(),
+      function () {
+        return jQuery().slick != null;
+      },
+      function () {
+        $(document).ready(function () {
+          $('.featuredPhotosSlider').slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+
+            autoplay: true,
+            autoplayspeed: 5000,
+          });
+        });
+
+        $('.ia-modal-inline-trigger-photo').magnificPopup({
+          type: 'inline',
+          preloader: false,
+          closeBtnInside: true,
+          showCloseBtn: true,
+          fixedBgPos: true,
+          mainClass: 'ak-photo-modal',
+        });
+        $('.ia-modal-inline-trigger-photo').on('click', function () {
+          $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        });
+      },
+      250
+    );
+};
+
+export const latestMediaWidget_callbacks = {
+  latestMediaWidget_data_loadPhotos
 }
--- site/src/js/library/modules/callbacks/NewsDetailWidget.js
+++ site/src/js/library/modules/callbacks/NewsDetailWidget.js
@@ -1,48 +1,51 @@
-export function newsDetailWidget_callbacks() {
-  // Rename in future
-  // NewsDetailWidget_ui
-  window.newsDetail_ui_prepSlider = function (_widget, _props) {
-    var profInfo1 = $('#profInfoLink1');
-    var profInfo2 = $('#profInfoLink2');
-    var profPhoto1 = $('#profPhotoLink2');
-    var profPhoto2 = $('#profPhotoLink2');
+// Rename in future
+// NewsDetailWidget_ui
+const newsDetail_ui_prepSlider = function (_widget, _props) {
+  var profInfo1 = $('#profInfoLink1');
+  var profInfo2 = $('#profInfoLink2');
+  var profPhoto1 = $('#profPhotoLink2');
+  var profPhoto2 = $('#profPhotoLink2');
 
-    if (profInfo1.length > 0 && profPhoto1.length > 0) {
-      window.GetNCIProfileInfoUrl('#profInfoLink1');
-      window.GetNCIProfilePictureUrl('#profPhotoLink1');
-    }
-    if (profInfo2.length > 0 && profPhoto2.length > 0) {
-      window.GetNCIProfileInfoUrl('#profInfoLink2');
-      window.GetNCIProfilePictureUrl('#profPhotoLink2');
-    }
-  };
+  if (profInfo1.length > 0 && profPhoto1.length > 0) {
+    window.GetNCIProfileInfoUrl('#profInfoLink1');
+    window.GetNCIProfilePictureUrl('#profPhotoLink1');
+  }
+  if (profInfo2.length > 0 && profPhoto2.length > 0) {
+    window.GetNCIProfileInfoUrl('#profInfoLink2');
+    window.GetNCIProfilePictureUrl('#profPhotoLink2');
+  }
+};
 
-  // Rename in future
-  // NewsDetailWidget_data
-  window.newsDetail_data_addContactInfo = function (data) {
-    var profInfo1 = $('#profInfoLink1');
-    var profInfo2 = $('#profInfoLink2');
-    var profPhoto1 = $('#profPhotoLink2');
-    var profPhoto2 = $('#profPhotoLink2');
-    var contact1Elements = [profInfo1, profPhoto1];
-    var contact2Elements = [profInfo2, profPhoto2];
+// Rename in future
+// NewsDetailWidget_data
+const newsDetail_data_addContactInfo = function (data) {
+  var profInfo1 = $('#profInfoLink1');
+  var profInfo2 = $('#profInfoLink2');
+  var profPhoto1 = $('#profPhotoLink2');
+  var profPhoto2 = $('#profPhotoLink2');
+  var contact1Elements = [profInfo1, profPhoto1];
+  var contact2Elements = [profInfo2, profPhoto2];
 
-    if (profInfo1.length > 0 && profPhoto1.length > 0) {
-      contact1Elements.forEach(function (profElement) {
-        profElement.attr(
-          'data-manager',
-          data.newsarticle.NCIContactPerson1.EMail
-        );
-      });
-    }
-    if (profInfo2.length > 0 && profPhoto2.length > 0) {
-      contact2Elements.forEach(function (profElement) {
-        profElement.attr(
-          'data-manager',
-          data.newsarticle.NCIContactPerson2.EMail
-        );
-      });
-    }
-    return data;
-  };
+  if (profInfo1.length > 0 && profPhoto1.length > 0) {
+    contact1Elements.forEach(function (profElement) {
+      profElement.attr(
+        'data-manager',
+        data.newsarticle.NCIContactPerson1.EMail
+      );
+    });
+  }
+  if (profInfo2.length > 0 && profPhoto2.length > 0) {
+    contact2Elements.forEach(function (profElement) {
+      profElement.attr(
+        'data-manager',
+        data.newsarticle.NCIContactPerson2.EMail
+      );
+    });
+  }
+  return data;
+};
+
+export const newsDetailWidget_callbacks = {
+  newsDetail_ui_prepSlider,
+  newsDetail_data_addContactInfo
 }
