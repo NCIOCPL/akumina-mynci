7be891f85bb7c4f16fc48b9b8afb0c536740c17c
Bela Olson <bela.olson@nih.gov>
2025-04-30 16:12:56 -0400

    (#198) Update Foundation News to show date posted not "Days Ago"
    * Moved blogdetail dateformat function into helper/Formatting.js
    * Called new function in both blogdetail and newsdetail
    
    Closes #198

--- site/src/js/library/modules/callbacks/NewsDetailWidget.js
+++ site/src/js/library/modules/callbacks/NewsDetailWidget.js
@@ -1,9 +1,20 @@
 import { nciProfileInfo } from "../helpers/NciProfileInfo";
+import { formatting } from "../helpers/Formatting";
 
 const newsDetail_ui_prepContacts = function (widget, _props) {
-  nciProfileInfo.updateProfileLinks(widget)
+  nciProfileInfo.updateProfileLinks(widget);
 };
+
+const newsDetail_data_changeDate = function (data) {
+  if (data.newsarticle.publisheddate != '') {
+    var formattedDate = formatting.dateFormat(data.newsarticle.Publish_x0020_Date);
+    data.newsarticle.publisheddate = formattedDate;
+  }
+  
+  return data;
+}
 
 export const newsDetailWidget_callbacks = {
   newsDetail_ui_prepContacts,
+  newsDetail_data_changeDate,
 }
